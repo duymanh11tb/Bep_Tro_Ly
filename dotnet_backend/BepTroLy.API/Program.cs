f3bee97cdc75408e28e7ccd1dd84b82911e52ac1
duymanh07 <doanduymanh11@gmail.com>
2026-03-21 17:20:37 +0700

    Update new v2

--- dotnet_backend/BepTroLy.API/Program.cs
+++ dotnet_backend/BepTroLy.API/Program.cs
@@ -34,7 +34,32 @@ builder.Services.AddSignalR();
 
 // Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Nhập JWT Token của bạn vào đây (không cần gõ chữ Bearer)"
+    });
+    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 // Database (MySQL/TiDB via Pomelo)
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
